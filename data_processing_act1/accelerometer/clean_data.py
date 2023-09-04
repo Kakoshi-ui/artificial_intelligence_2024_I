5887727489e95676d8ef1909e8c81ba5082c45a5
Kakoshi <axel93rr30@gmail.com>
2023-09-04 12:20:47 -0600

    First Commit

---
+++ data_processing_act1/accelerometer/clean_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import os
+
+datos_completos = pd.read_csv("C:/Users/axel9/Documents/Intelligent_Sys/accelerometer/accelerometer.csv")
+datos_completos.head(10)
